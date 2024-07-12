26249dda767dc7ba8488e04adee1dfdefa89b652
Atilla ÇALIKUŞU <calikusu.atilla@gmail.com>
2024-07-12 07:24:18 +0300

    activity_cart xml kodları birildi

--- app/src/main/java/com/example/tecnoshop/Activity/CartActivity.kt
+++ app/src/main/java/com/example/tecnoshop/Activity/CartActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tecnoshop.R
+import com.example.tecnoshop.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_cart)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityCartBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
     }
 }
